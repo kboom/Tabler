17e82b46daf4dabd8dc09ac278f041a8589b2391
kboom <gurgul.grzegorz@gmail.com>
2016-12-03 19:03:29 +0100

    Introduced three js template scene.

--- src/main/pages/placement/placement.js
+++ src/main/pages/placement/placement.js
@@ -1,12 +1,13 @@
 import React, {PropTypes} from "react";
 import Layout from "../../layout/Layout";
+import Scene from "../../rendering/scenes/Scene"
 
 export default class HomePage extends React.Component {
 
     render() {
         return (
             <Layout>
-                abc
+                <Scene />
             </Layout>
         );
     }
